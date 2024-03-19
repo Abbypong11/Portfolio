1b0f0542585e4250da00181e4b287ba48f3d984b
Abbypong11 <abbypong11@gmail.com>
2024-03-19 15:33:59 +0000

    backend initialization

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express  from "express";
+import cors from  "cors";
+import bodyParser from "body-parser";
+import skills from "./routes/skills.routes.js"
+const PORT = process.env.PORT || 4000;
+
+const app =  express();
+//use middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//load routes
+app.use("/skills", skills);
+
+//start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ Backend/routes/skills.routes.js
@@ -0,0 +1,55 @@
+import { Router } from "express";
+import db from "../db/connection.js";
+import { ObjectId } from "mongodb";
+
+const router = Router();
+const SKILLS_COLLECTION = db.collection("skills");
+
+//Endpoint for getting lists of skills
+router.get("/", async (req, res) => {
+  let results = await SKILLS_COLLECTION.find({}).toArray();
+  res.send(results).status(200);
+});
+
+//Endpoint for getting  a single skill by its id
+router.get("/:id", async (req, res) => {
+ let query ={ _id: new ObjectId (req.params.id)};
+ let result = await SKILLS_COLLECTION.findOne(query);
+!result? res.send("Not found").status(404) : res.send(result).status(200);
+});
+
+//Endpoint for  adding a new skill to the database
+router.post("/", async (req, res) => {
+  try {
+    let newSkill = {
+      skill: req.body.skill,
+      proficiency: req.body.proficiency,
+    };
+    let result = await SKILLS_COLLECTION.insertOne(newSkill);
+    res.send(result).status(201);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//Endpoint for updating a skill by id
+router.patch("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+    const update = {
+      $set: {
+        skill: req.body.skill,
+        proficiency: req.body.proficiency,
+      },
+    };
+    let result = await SKILLS_COLLECTION.updateOne(query, update);
+    res.send(result).status(201);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+//Endpoint for deleting a skill by id
+router.delete
+
+export default router;
